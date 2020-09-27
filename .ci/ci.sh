35f24006563856e171c55b338af88c0a65217acd
liushuyu <liushuyu011@gmail.com>
2020-09-26 22:03:16 -0600

    ci: fix script permissions

--- .ci/ci.sh
+++ .ci/ci.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ZOLA_VERSION='0.12.0'
 
+echo 'Installing Zola ...'
 wget -q https://github.com/getzola/zola/releases/download/v"$ZOLA_VERSION"/zola-v"$ZOLA_VERSION"-x86_64-unknown-linux-gnu.tar.gz
-tar xf zola-v"$ZOLA_VERSION"-x86_64-unknown-linux-gnu.tar.gz
-sudo cp zola /usr/local/bin/
+tar xvf zola-v"$ZOLA_VERSION"-x86_64-unknown-linux-gnu.tar.gz
+sudo install -Dvm755 zola /usr/local/bin/
